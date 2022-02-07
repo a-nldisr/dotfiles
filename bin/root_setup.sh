7b0f7de40a59dd1e5114e1dae03b1eddc624b2e1
a-nldisr <1867776+a-nldisr@users.noreply.github.com>
2022-02-07 14:34:17 +0100

    setup_sudoers is now conditional

--- bin/root_setup.sh
+++ bin/root_setup.sh
@@ -13,22 +13,24 @@ check_sudo() {
 
 check_sudo_user() {
     # This assumes SUDO_USER is set, which is default on mac. Else we assume there is only 1 user on this system. Its not perfect.
-    if [ "$SUDO_USER" ]; then 
+    if [ "$SUDO_USER" ]; then
         USERNAME=$SUDO_USER
-    else 
+    else
         USERNAME=$(find /Users/* -maxdepth 0 -printf "%f" -type d || echo "$USER")
     fi
 }
 
 setup_sudoers() {
-    # I know what im doing, but when i don't lock my pc that one time in history... Please kick the shit out of me.
-    { \
-        # shellcheck disable=SC2086
-		echo -e 'Defaults	secure_path="/usr/local/go/bin:/Users/'${USERNAME}'/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'; \
-		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
-		echo -e "${USERNAME} ALL=(ALL) NOPASSWD:ALL"; \
-		echo -e "${USERNAME} ALL=NOPASSWD: /usr/sbin/installer"; \
-	} >> /etc/sudoers
+    # I know what im doing, but when i don't lock my pc: Please kick the shit out of me.
+    if grep -q "${USERNAME}" /etc/sudoers; then
+        { \
+            # shellcheck disable=SC2086
+		    echo -e 'Defaults	secure_path="/usr/local/go/bin:/Users/'${USERNAME}'/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'; \
+		    echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
+		    echo -e "${USERNAME} ALL=(ALL) NOPASSWD:ALL"; \
+		    echo -e "${USERNAME} ALL=NOPASSWD: /usr/sbin/installer"; \
+	    } >> /etc/sudoers
+    fi
 }
 
 main() {
